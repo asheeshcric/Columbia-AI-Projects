6607cce89326f92548fb45bfe6fdbceca5bd4e8c
asheeshcric <ashiz2013@gmail.com>
2018-04-15 11:34:54 +0545

    [COMPLETE] BFS and DFS algorithm to solve an 8-puzzle problem

--- driver.py
+++ driver.py
@@ -39,7 +39,6 @@ class State:
         # print('allowed_operations - ', allowed_operations)
         for operation in allowed_operations:
             new_state = list(self.state)
-            # print('current_state - ', new_state)
             zero_position = tuple(self.state).index(0)
             if operation == Operations.up:
                 new_zero_postion = zero_position - 3
@@ -52,16 +51,9 @@ class State:
 
             new_state[zero_position] = new_state[new_zero_postion]
             new_state[new_zero_postion] = 0
-            # print('new_state - ', new_state)
             available_neighbors.append(State(tuple(new_state), self, operation, self.cost_of_path + 1))
 
         return available_neighbors
-
-
-
-
-class Solver:
-    pass
 
 
 def write_to_file(path_to_goal, cost_of_path, nodes_expanded, search_depth, max_search_depth, running_time, max_ram_usage):
@@ -88,7 +80,8 @@ def bfs(initial_state):
 
     while frontier:
         state = frontier.popleft()
-        explored.add(state)
+        state_being_checked = state.state
+        explored.add(state.state)
 
         if state.check_goal_state():
             path_to_goal = []
@@ -104,7 +97,6 @@ def bfs(initial_state):
             running_time = time.clock() - start_time
             import resource
             max_ram_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-            print(path_to_goal, cost_of_path, search_depth, running_time, max_ram_usage)
             write_to_file(path_to_goal, cost_of_path, nodes_expanded, search_depth, max_search_depth, running_time, max_ram_usage)
             return
             # Calculate ram usage and write output to file
@@ -113,25 +105,68 @@ def bfs(initial_state):
         nodes_expanded += 1
 
         for neighbor in state.neighbors():
-            if not neighbor in explored:
+            neighbor_being_checked = neighbor.state
+            if neighbor.state not in explored:
                 frontier.append(neighbor)
-                explored.add(neighbor)
+                explored.add(neighbor.state)
+                max_search_depth = max(max_search_depth, neighbor.cost_of_path)
+
+    print("The problem did not result to a solution!")
+
+
+
+
+def dfs(initial_state):
+    start_time = time.clock()
+
+    frontier = deque([initial_state])
+    explored = set()
+
+    nodes_expanded = 0
+    max_search_depth = -1
+
+    while frontier:
+        state = frontier.pop()
+        state_being_checked = state.state
+        explored.add(state.state)
+
+        if state.check_goal_state():
+            path_to_goal = []
+            current_state = state
+
+            while current_state.parent_state:
+                path_to_goal.append(current_state.operation_on_parent)
+                current_state = current_state.parent_state
+
+            path_to_goal = path_to_goal[::-1]  # Reversing the order of path list
+            cost_of_path = len(path_to_goal)
+            search_depth = state.cost_of_path
+            running_time = time.clock() - start_time
+            import resource
+            max_ram_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            write_to_file(path_to_goal, cost_of_path, nodes_expanded, search_depth, max_search_depth, running_time,
+                          max_ram_usage)
+            return
+            # Calculate ram usage and write output to file
+
+        # The output cannot be derived from the current node. So, it should be expanded
+        nodes_expanded += 1
+
+        for neighbor in state.neighbors()[::-1]:        # Reversing the list of neighbors so that popping results in UDLR
+            neighbor_being_checked = neighbor.state
+            if neighbor.state not in explored:
+                frontier.append(neighbor)
+                explored.add(neighbor.state)
                 max_search_depth = max(max_search_depth, neighbor.cost_of_path)
 
     print("The given problem has no solution!!!")
 
-
-
-
-def dfs(state):
-    pass
-
-def ast(state):
+def ast(initial_state):
     pass
 
 if __name__ == '__main__':
     input_arguments = sys.argv[1:]
-    # print(type(input_arguments))
+    # print((input_arguments))
     search_type = input_arguments[0]
     input_state = input_arguments[1]
     initial_state = tuple((int(x) for x in input_state.split(',')))    # Using tuple to represent a state
