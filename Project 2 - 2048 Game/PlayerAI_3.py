0e7e3c2e6e622b7216efd6e8ef229b129ad7d69e
asheeshcric <ashiz2013@gmail.com>
2018-04-18 08:30:02 +0545

    [TEST] First test of project 2 from skeleton code using random decisions for player

--- Project 2 - 2048 Game/PlayerAI_3.py
+++ Project 2 - 2048 Game/PlayerAI_3.py
@@ -1,1 +1,10 @@
-# Script that plays the game
+from random import randint
+
+from BaseAI_3 import BaseAI
+
+
+class PlayerAI(BaseAI):
+
+    def getMove(self, grid):
+        moves = grid.getAvailableMoves()
+        return moves[randint(0, len(moves) - 1)] if moves else None
