6cc5c74717a73db12e3a2d380b1ccb08e6f84f28
asheeshcric <ashiz2013@gmail.com>
2018-04-20 22:46:48 +0545

    [COMPLETE] Project 5 - NLP using sklearn and pandas

--- Project 5 - NLP/driver_3.py
+++ Project 5 - NLP/driver_3.py
@@ -1,141 +1,125 @@
-import numpy as np
-import pandas as pd
-import glob
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+import csv
+import os
+import pandas as pd
 
 
 
-train_path = "./aclImdb/" # use terminal to ls files under this directory
-test_path = "./imdb_te.csv" # test data for grade evaluation
 
-#train_path = "../resource/asnlib/public/aclImdb/" # use terminal to ls files under this directory
-#test_path = "../resource/asnlib/public/imdb_te.csv" # test data for grade evaluation
+def imdb_data_preprocess(text):
+    global set_stop_word
+    replaced_text = text.replace('<br />', ' ')     # Remove chars with space
+    chars = ".,?!;:-_/<>~`@#$%^&*()+=[]{}|\\\"\'"   # String used for removing punctuations
+    for c in chars:
+        replaced_text = replaced_text.replace(c, ' ')
+    replaced_text_list = replaced_text.split()
+    pText = ' '.join([w for w in replaced_text_list if w.lower() not in set_stop_word])
 
-
-# uncomment this before uploading the file
-# train_path = "../resource/asnlib/public/aclImdb/train/" # use terminal to ls files under this directory
-# test_path = "../resource/asnlib/public/imdb_te.csv" # test data for grade evaluation
-
-
-def classfier(inpath="./", name="imdb_tr.csv", ngram=1, outfile="./output.txt", tfidf=False):
-
-    #load vocablary
-    #f = open(train_path + 'imdb.vocab', 'r')
-    #vocab_words = list(f)
-    #vocab_words = [w[:-1] for w in vocab_words]
-    #f.close()
+    return pText
 
 
-    #load train dataset
-    data = pd.read_csv(inpath+name)
+def dataPreprocess(inpath, name):
+    data_folders = ['pos/', 'neg/']
+    out_file = open(name, 'w', encoding='utf-8', newline='')
+    out_writer = csv.writer(out_file)
+    out_writer.writerow(['', 'text', 'polarity'])
+    i = -1
+    for folder in data_folders:
+        current_path = inpath + folder
+        for r, d, fns in os.walk(current_path):
+            for fn in fns:
+                i = i + 1
+                tk = fn.split('_')
+                rating = tk[1].split('.')
+                with open(current_path + fn, 'r', encoding="utf-8") as inp_file:
+                    text = inp_file.read()
+                pText = imdb_data_preprocess(text)
+                out_writer.writerow([i, pText, int(int(rating[0]) > 5)])
+    out_file.close()
 
-    train_data, val_data = train_test_split(data, test_size=0.1, random_state=32)
 
-    if not tfidf:
-        #vectorizer = CountVectorizer(vocabulary=vocab_words, stop_words='english', ngram_range=(ngram,ngram))
-        vectorizer = CountVectorizer(stop_words='english', ngram_range=(ngram,ngram))
+def trainTDM(trainData, ngram, tfidf):
+    global list_stop_word
+    if tfidf:
+        vectorizer = TfidfVectorizer(ngram_range=(1, ngram), stop_words=list_stop_word)
     else:
-        #vectorizer = TfidfVectorizer(vocabulary=vocab_words, stop_words='english', ngram_range=(ngram,ngram))
-        vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(ngram,ngram))
+        vectorizer = CountVectorizer(ngram_range=(1, ngram), stop_words=list_stop_word)
+    termDocMatrix = vectorizer.fit_transform(trainData['text'])
+    vocabulary = vectorizer.vocabulary_
+    clf = SGDClassifier(loss='hinge', penalty='l1')
+    clf.fit(termDocMatrix, trainData['polarity'])
 
-    X_train = vectorizer.fit_transform(train_data['text'].tolist())
-    Y_train = train_data['polarity'].tolist()
-
-    X_val = vectorizer.transform(val_data['text'].tolist())
-    Y_val = val_data['polarity'].tolist()
-
-
-    clf = SGDClassifier(loss='hinge', penalty='l2')
-
-    clf.fit(X_train, Y_train)
-
-    #check performance on validation set
-    val_predict = clf.predict(X_val)
-
-    train_predict = clf.predict(X_train)
-
-    print ("Train Accracy={}".format(accuracy_score(Y_train, train_predict)))
-    print ("Validation Accracy={}".format(accuracy_score(Y_val, val_predict)))
-
-    # load test dataset
-    test_data = pd.read_csv(test_path, encoding='ISO-8859-1')
-    X_test = vectorizer.transform(test_data['text'].tolist())
-
-    #predict
-    test_predict = clf.predict(X_test)
-
-    f = open(outfile, 'w')
-    for item in test_predict:
-        f.write("%d\n" % item)
-    f.close()
+    return vocabulary, clf
 
 
+def testTDM(test_data, ngram, vocabulary, clf, tfidf):
+    global list_stop_word
+    if tfidf:
+        tf_vectorizer = TfidfVectorizer(ngram_range=(1, ngram), stop_words=list_stop_word, vocabulary=vocabulary)
+    else:
+        tf_vectorizer = CountVectorizer(ngram_range=(1, ngram), stop_words=list_stop_word, vocabulary=vocabulary)
+    term_doc_matrix = tf_vectorizer.fit_transform(test_data['text'])
+    result_list = clf.predict(term_doc_matrix)
 
-def imdb_data_preprocess(inpath, outpath="./", name="imdb_tr.csv", mix=False):
-    '''Implement this module to extract
-	and combine text files under train_path directory into 
-    imdb_tr.csv. Each text file in train_path should be stored 
-    as a row in imdb_tr.csv. And imdb_tr.csv should have two 
-    columns, "text" and label'''
+    return result_list
 
-    inpath += 'train/'
-    f = open('./stopwords.en.txt', 'r')
-    stopwords = list(f)
-    stopwords = [w[:-1] for w in stopwords]
-    f.close()
 
-    # get texts with +ve sentiments
-    comments=[]
-    labels =[]
-    for file in glob.glob(inpath+"pos/*.txt"):
-        text_file = open(file, "r")
-        line = text_file.read()
+def writeToFile(fn, result_list):
+    with open(fn, 'w', newline='') as out_file:
+        out_writer = csv.writer(out_file)
+        for result in result_list:
+            out_writer.writerow([result])
 
-        # remove stopwords
-        #line = ' '.join(filter(lambda x: x.lower() not in stopwords, line.split()))
 
-        comments.append(line)
-        labels.append(1)
-        text_file.close()
+def start_program():
+    global set_stop_word
+    global list_stop_word
 
-    # get texts with -ve sentiments
-    for file in glob.glob(inpath+"neg/*.txt"):
-        text_file = open(file, "r")
-        line = text_file.read()
-        comments.append(line)
-        labels.append(0)
-        text_file.close()
+    train_path = "../resource/lib/publicdata/aclImdb/train/"  # use terminal to ls files under this directory
+    test_path = "../resource/lib/publicdata/imdb_te.csv"  # test data for grade evaluation
+    complete_train_path = "imdb_tr.csv"
 
-    df = pd.DataFrame.from_dict({'text':comments, 'polarity':labels})
-    df = df[['text','polarity']]
+    with open("stopwords.en.txt", 'r') as inputfile:
+        list_stop_word = inputfile.read().splitlines()
+    set_stop_word = set(list_stop_word)
 
-    df.to_csv(outpath+name)
+    dataPreprocess(inpath=train_path, name=complete_train_path)
+    trainData = pd.read_csv(complete_train_path, encoding="utf-8")
+    testData = pd.read_csv(test_path, encoding="ISO-8859-1")
+    testData['text'] = testData['text'].apply(imdb_data_preprocess)
+
+    # Unigram Prediction
+    vocabulary, clf = trainTDM(trainData, ngram=1, tfidf=False)
+    result_list = testTDM(testData, ngram=1, vocabulary=vocabulary, clf=clf, tfidf=False)
+    writeToFile("unigram.output.txt", result_list)
+
+
+    # Bigram Prediction
+    vocabulary, clf = trainTDM(trainData, ngram=2, tfidf=False)
+    result_list = testTDM(testData, ngram=2, vocabulary=vocabulary, clf=clf, tfidf=False)
+    # Print results
+    writeToFile("bigram.output.txt", result_list)
+
+
+
+    # Unigram with Tfidf Prediction
+    vocabulary, clf = trainTDM(trainData, ngram=1, tfidf=True)
+    result_list = testTDM(testData, ngram=1, vocabulary=vocabulary, clf=clf, tfidf=True)
+    writeToFile("unigramtfidf.output.txt", result_list)
+
+
+
+    # Bigram with Tfidf Prediction
+    vocabulary, clf = trainTDM(trainData, ngram=2, tfidf=True)
+    result_list = testTDM(testData, ngram=2, vocabulary=vocabulary, clf=clf, tfidf=True)
+    # Print results
+    writeToFile("bigramtfidf.output.txt", result_list)
+    # ---------------------------------#
 
 
 if __name__ == "__main__":
-
-    #imdb_data_preprocess(train_path)
-
-    '''train a SGD classifier using unigram representation,
-    predict sentiments on imdb_te.csv, and write output to
-    unigram.output.txt'''
-    classfier(inpath = "./", name = "imdb_tr.csv", ngram = 1, outfile="./unigram.output.txt", tfidf = False)
-
-    '''train a SGD classifier using bigram representation,
-    predict sentiments on imdb_te.csv, and write output to
-    unigram.output.txt'''
-    classfier(inpath = "./", name = "imdb_tr.csv", ngram = 2, outfile="./bigram.output.txt", tfidf = False)
-
-    '''train a SGD classifier using unigram representation
-    with tf-idf, predict sentiments on imdb_te.csv, and write 
-    output to unigram.output.txt'''
-    classfier(inpath = "./", name = "imdb_tr.csv", ngram = 1, outfile="./unigramtfidf.output.txt", tfidf = True)
-
-    '''train a SGD classifier using bigram representation
-    with tf-idf, predict sentiments on imdb_te.csv, and write 
-    output to unigram.output.txt'''
-    classfier(inpath = "./", name = "imdb_tr.csv", ngram = 2, outfile="./bigramtfidf.output.txt", tfidf = True)
+    start_program()
